f0b4c8703284f88be9481d56f6ab88343a49d464
Varma0214 <156598466+Varma0214@users.noreply.github.com>
2024-11-19 22:39:10 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,11 @@ const cors = require('cors');
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true}
+            ));
 require("./models/db")
 app.use(express.json());
 app.use('/api/resources', resourceRoutes);
